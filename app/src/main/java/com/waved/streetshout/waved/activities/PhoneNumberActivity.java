714540dddd36e7fcb34a0d3aae89ee27ec1e15e8
Bastien Beurier <bastienbeurier@gmail.com>
2014-11-05 18:39:38 -0800

    Fix launch icon

--- app/src/main/java/com/waved/streetshout/waved/activities/PhoneNumberActivity.java
+++ app/src/main/java/com/waved/streetshout/waved/activities/PhoneNumberActivity.java
@@ -2,8 +2,6 @@ package com.waved.streetshout.waved.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import com.waved.streetshout.waved.R;
 

f9438f19fb799d60cc8293751dd2b6da08bf3746
Bastien Beurier <bastienbeurier@gmail.com>
2014-11-05 18:25:08 -0800

    First commit

---
+++ app/src/main/java/com/waved/streetshout/waved/activities/PhoneNumberActivity.java
@@ -0,0 +1,17 @@
+package com.waved.streetshout.waved.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.waved.streetshout.waved.R;
+
+public class PhoneNumberActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.phone_number);
+    }
+}
---
+++ app/src/main/java/com/waved/streetshout/waved/activities/WelcomeActivity.java
@@ -0,0 +1,32 @@
+package com.waved.streetshout.waved.activities;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.waved.streetshout.waved.R;
+
+public class WelcomeActivity extends Activity {
+
+    private Button startButton = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome);
+
+        startButton = (Button) findViewById(R.id.welcome_button);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(WelcomeActivity.this, PhoneNumberActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
